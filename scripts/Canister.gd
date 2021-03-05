0d792bbf80fb1e1d78740b823c56e78e72119f8c
Frank Homolka <frank62899@gmail.com>
2021-03-04 18:16:26 -0800

    new stuff

--- scripts/Canister.gd
+++
@@ -1,5 +0,0 @@
-extends Area
-
-
-func _ready():
-	pass
--- scripts/Lobby.gd
+++ scripts/Lobby.gd
@@ -11,12 +11,12 @@ func _on_ButtonHost_pressed():
 func _on_ButtonJoin_pressed():
 	var net = NetworkedMultiplayerENet.new()
 	# change ip for multiple computers
-	net.create_client("192.168.1.5", 6969)
+	net.create_client("2600:6c52:7a00:b6f:4187:f248:806c:8fa4", 6969) #192.168.1.5
 	get_tree().set_network_peer(net)
 
 func _player_connected(id):
 	print('player connected')
 	Globals.player2id = id
-	var game = preload("res://assets/HeightmapTest.tscn").instance() #
+	var game = preload("res://assets/MainLevel.tscn").instance() #
 	get_tree().get_root().add_child(game)
 	hide()
