3d7bd1d304c1e7d6485afb214320d8aeef822fe8
Frank Homolka <frank62899@gmail.com>
2021-03-08 15:06:11 -0800

    idk

--- scripts/Lobby.gd
+++ scripts/Lobby.gd
@@ -5,16 +5,13 @@ func _ready():
 
 func _on_ButtonHost_pressed():
 	var net = NetworkedMultiplayerENet.new();
-	net.create_server(7777, 2)
+	net.create_server(6969, 2)
 	get_tree().set_network_peer(net)
-	var upnp = UPNP.new()
-	upnp.discover(2000, 2, "InternetGatewayDevice")
-	upnp.add_port_mapping(7777)
 
 func _on_ButtonJoin_pressed():
 	var net = NetworkedMultiplayerENet.new()
 	# change ip for multiple computers
-	net.create_client("192.168.1.5", 7777) #192.168.1.5
+	net.create_client("47.7.49.50", 6969) #192.168.1.5
 	get_tree().set_network_peer(net)
 
 func _player_connected(id):
