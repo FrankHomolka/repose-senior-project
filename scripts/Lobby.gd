595854ed741931170decfcfb57e567327a14a3ad
Frank Homolka <frank62899@gmail.com>
2021-03-30 10:50:44 -0700

    Tutorial, new map, recolored map, mountains, IP input, bug fixes

--- scripts/Lobby.gd
+++ scripts/Lobby.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var serverIp = "192.168.1.5"
+
 func _ready():
 	get_tree().connect("network_peer_connected", self, "_player_connected")
 
@@ -11,7 +13,7 @@ func _on_ButtonHost_pressed():
 func _on_ButtonJoin_pressed():
 	var net = NetworkedMultiplayerENet.new()
 	# change ip for multiple computers
-	net.create_client("192.168.1.7", 6969)
+	net.create_client(serverIp, 6969)
 	# local - 192.168.1.5  
 	# internet - 47.7.49.50
 	get_tree().set_network_peer(net)
@@ -22,3 +24,7 @@ func _player_connected(id):
 	var game = preload("res://assets/MainLevel.tscn").instance() #
 	get_tree().get_root().add_child(game)
 	hide()
+
+
+func _on_IpInput_text_changed(new_text):
+	serverIp = new_text
